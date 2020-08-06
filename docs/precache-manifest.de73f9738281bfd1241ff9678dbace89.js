2ec7f5834f3c41b3d825e35ac48bf2ccb5b2a85d
Alfredo <dominiolibre@gmail.com>
2020-08-05 22:36:42 -0300

    Docs agregador

---
+++ docs/precache-manifest.de73f9738281bfd1241ff9678dbace89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e67374526ed6344cfd082643ba1fc2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fc369f9293b6cffba32",
+    "url": "/static/css/main.6b91ea96.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc369f9293b6cffba32",
+    "url": "/static/js/main.267fd7af.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
